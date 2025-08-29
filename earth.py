10c96c49808c9257594bb95c8f895b12a10f37a4
Manuel Couto <mcouto1@mail.austral.edu.ar>
2025-08-29 12:33:40 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,12 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    isXlower=x<y
+    isYlower=y<x
+
+    message1=f"The result of {x} comes first in the dictionary than {y} is {isXlower}."
+    message2=f"The result of {y} comes first in the dictionary than {x} is {isYlower}."
+
+    print(message1)
+    print(message2)
